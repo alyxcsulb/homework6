85a81e6fb1517788494030a907ab1e5e76f3f4ad
Alyx Faber <alyx.faber@student.csulb.edu>
2020-11-19 18:25:40 -0800

    initial commit

---
+++ task1.py
@@ -0,0 +1,8 @@
+# task 1
+# output should have the following:
+# Total word count
+# Total character count
+# The average word length
+# The average sentence length
+# A word distribution of all words ending in “ly”
+# A list of top 10 longest words.
---
+++ task2.py
@@ -0,0 +1,4 @@
+# task 2
+# case in-sensiive (a= A)
+# output should show letters in alphabetical order
+
