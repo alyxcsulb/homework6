e0435bd4eb1d73944dcad2937383f2545c90fe11
Alyx Faber <alyx.faber@student.csulb.edu>
2020-11-20 22:23:14 -0800

    in progress

--- task1.py
+++ task1.py
@@ -6,3 +6,53 @@
 # The average sentence length
 # A word distribution of all words ending in “ly”
 # A list of top 10 longest words.
+
+import os
+
+READ_FILE_NAME = 'book1.txt'
+READ_MODE = 'r'
+WRITE_FILE_NAME = 'summary1.txt'
+WRITE_MODE = 'w'
+
+word_count = 0
+total_char = 0
+avg_sentence = 0
+ly_words = 0
+top_ten = 0
+
+try:
+    book_file = open(READ_FILE_NAME, READ_MODE)
+    write_file = open(WRITE_FILE_NAME, WRITE_MODE)
+
+    with write_file, book_file:
+        lines = book_file.read()    
+        
+        # word count
+        word_count = lines.split()
+        write_file.write(f'Total word count: {len(word_count)}\n')
+
+        #character count
+        total_char += sum(len(char) for char in word_count)
+        write_file.write(f'Total character count: {len(total_char)}\n')
+
+        #Average word length
+        avg_length = 0
+        for word in word_count:
+            char = len(word)
+            total += char
+        avg_length = total / len(word_count)
+        write_file.write(f'The average word length is: {int(avg_length)}\n')
+        
+        # Average sentence length 
+        avg_sentence = lines.split('.')
+        write_file.write(f'The average sentence length is: {int(total / len(avg_sentence))}\n')
+        
+        # Word distribution of all words ending in "ly"
+        # come back to 
+        write_file.write('A word distribution of all words ending in "ly" \n')
+   
+        # List of top 10 longest words in descending order
+        # come back to
+        write_file.write('A list of top 10 longest words \n')
+except:
+    print(f'File {READ_FILE_NAME} not found.')
--- task2.py
+++ task2.py
@@ -1,4 +1,40 @@
 # task 2
 # case in-sensiive (a= A)
 # output should show letters in alphabetical order
+# include dictionary
 
+import os
+
+READ_SENTENCE = 'book2.txt'
+READ_MODE = 'r'
+WRITE_LETTERS = 'summary2.txt'
+WRITE_MODE = 'w'
+letters = []
+frequency = []
+
+try:
+    with open (READ_SENTENCE, READ_MODE) as content:
+        for line in content:
+            content = line.rstrip('\n')
+            for letter in content:
+                letter = letter.lower()
+                if letter not in letters and ord(letter) >= 97 and ord(letter) <= 122:
+                    letters += [letter]
+                    frequency += [[letter,1]]
+                else:
+                    for bracket in frequency:
+                        if letter in bracket [0]:
+                            frequency[frequency.index(bracket)][1] += 1
+    letters.sort()
+
+    with open (WRITE_LETTERS, WRITE_MODE) as text:
+        for letter in letters:
+            for bracket in frequency:
+                if letter == bracket[0]:
+                    text.write(f'{bracket[0].upper()} {bracket[1]}\n')
+            if len(letters) == 26:
+                text.write(f'It does have all letters ')
+            else:
+                text.write(f'It does not have all letters ')
+except:
+    print(f'File {READ_SENTENCE} not found.')                                                        
